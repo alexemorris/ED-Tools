113b04ebbf1c488c83d638cd1a6eb0bdd0a0056e
alexemorris <alex.morris@ravn.co.uk>
2017-12-07 17:08:15 +0000

    fixed some relative links

--- ED-Tools/ED-Tools.psm1
+++ ED-Tools/ED-Tools.psm1
@@ -1,17 +1,14 @@
 ################################################################################
-# Author     : Antony Onipko (antony.onipko@ravn.co.uk)
-# Company    : RAVN Systems Ltd.
-# Copyright  : (c) 2016 RAVN Systems Ltd. All rights reserved.
+# Author     : Alex Morris (alex@aemcreative.co.uk)
+# Company    : Financial Conduct Authority
 ################################################################################
 #  This work is licensed under a Creative Commons
 #  Attribution-NonCommercial-NoDerivatives 4.0 International License.
 #  http://creativecommons.org/licenses/by-nc-nd/4.0/
-#  Permissions beyond the scope of this license may be available at
-#  http://www.ravn.co.uk.
 ################################################################################
 
-Get-ChildItem -Path $psScriptRoot `
-    | ? { $_ -match '^Func_.+$' } `
+Get-ChildItem -Path $psScriptRoot + '\functions' `
+    | ? { $_ -match '^func_.+$' } `
     | % {
         . (Join-Path -Path $psScriptRoot -ChildPath $_)
     }
