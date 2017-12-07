48e4c2f49f9021850a7e9831d8b2c8c2d3231e42
alexemorris <alex.morris@ravn.co.uk>
2017-12-07 16:15:16 +0000

    updated readme - fixed repetition

--- build.ps1
+++ build.ps1
@@ -1,9 +1,1 @@
-param($inputdir, $outputdir, $release) 
-
-Write-Host $inputdir 
-Write-Host $outputdir
-Write-Host ($release -ne $true)
-
-
-rm -Recurse $outputdir
-mkdir  $outputdir
+#TODO MAKE THIS WORK!
